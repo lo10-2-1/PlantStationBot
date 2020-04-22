03b17487d72ae0b090151c6df24bb1c465e786e4
lo10-2-1 <lidrudakova@gmail.com>
2020-04-22 17:28:51 +0300

    Starting again

---
+++ main.py
@@ -0,0 +1,30 @@
+'''Links for help:
+https://github.com/python-telegram-bot/python-telegram-bot/blob/master/README.rst
+'''
+
+def main():
+    from telegram.ext import Updater, CommandHandler
+    import logging
+
+    updater = Updater(token='1157983371:AAEzM_LHvpEyLRHAGnpHZfa0tZkH_Nfm0H8', 
+                        use_context=True)
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    def start(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, 
+                            text="Привет! Теперь ты можешь воспользоваться мной.")
+    def helpme(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, 
+                            text="Чувак, подожди, помощи пока не жди.")
+        
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    helpme_handler = CommandHandler('help', helpme)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(helpme_handler)
+
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
