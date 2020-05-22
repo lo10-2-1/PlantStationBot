870ebb4d94d0704d146a1ac3c9854da98af834c1
lo10-2-1 <lidrudakova@gmail.com>
2020-05-22 17:27:04 +0300

    Get and bool funtions

--- dao.py
+++ dao.py
@@ -43,24 +43,32 @@ def add_user_by_telegram(telegram_id: int, first_name: str, last_name: str, logi
     session.commit()
 
 
-def change_user_role(user_id: int, role: int):
+def update_user_role(user_id: int, role: int):
     pass
 
 
-def is_plant_exists(plant_name: str) -> bool:
-    pass
+def is_plant_exists(plant_title: str) -> bool:
+    query = session.query(Plants) \
+        .filter(Plants.name.ilike('%{}%'.format(plant_title)))
+    return session.query(query.exists()).scalar()
 
 
-def get_plant_id_by_name(plant_name: str) -> int:
-    pass
+def get_plant_id_by_title(plant_title: str) -> int:
+    plant = session.query(Plants) \
+        .filter(Plants.name.ilike('%{}%'.format(plant_title))) \
+        .first()
+    return plant.plant_id
 
 
 def get_plant(plant_id: int):
-    pass
+    plant_all = session.query(Plants) \
+        .filter(Plants.plant_id == plant_id) \
+        .first()
+    return plant_all
 
 
-def add_plant(plant_name: str, description=None, light=None, temperature=None, watering=None, spraying=None, fertilizer=None, transfer=None, reproduction=None, photo_link=None):
-    session.add(Plants(name=plant_name,
+def add_plant(plant_title: str, description=None, light=None, temperature=None, watering=None, spraying=None, fertilizer=None, transfer=None, reproduction=None, photo_link=None):
+    session.add(Plants(name=plant_title,
                      description=description,
                      light=light,
                      temperature=temperature,
@@ -75,20 +83,30 @@ def add_plant(plant_name: str, description=None, light=None, temperature=None, w
     session.commit()
 
 
-def update_plant(plant_id: int, plant_name=None, description=None, light=None, temperature=None, watering=None, spraying=None, fertilizer=None, transfer=None, reproduction=None, photo_link=None):
+def update_plant(plant_id: int, plant_title=None, description=None, light=None, temperature=None, watering=None, spraying=None, fertilizer=None, transfer=None, reproduction=None, photo_link=None):
     pass
 
 
 def is_user_plant_exists(user_id: int, plant_name: str) -> bool:
-    pass
+    query = session.query(UsersPlants) \
+        .filter(UsersPlants.user_id == user_id, 
+                UsersPlants.name.ilike('%{}%'.format(plant_name)))
+    return session.query(query.exists()).scalar()
 
 
 def get_user_plants(user_id: int):
-    pass
+    user_plant = session.query(UsersPlants) \
+        .filter(UsersPlants.user_id == user_id) \
+        .all()
+    return user_plant
 
 
 def get_user_plant_id(user_id: int, plant_name: str) -> int:
-    pass
+    user_plant_id = session.query(UsersPlants) \
+        .filter(UsersPlants.user_id == user_id,
+                UsersPlants.name.ilike('%{}%'.format(plant_name))) \
+        .first()
+    return user_plant_id
 
 
 def add_plant_to_user(user_id: int, plant_name: str, created: str):
@@ -104,32 +122,54 @@ def delete_user_plant(user_id: int, plant_name: str):
     pass
 
 
-def get_notif_categories(user_id: int):
-    pass
+def get_notif_categories():
+    notif_categories = session.query(NotificationCategory.category) \
+        .order_by(NotificationCategory.id) \
+        .all()
+    return notif_categories
 
 
 def get_notif_category_id(category: str) -> int:
-    pass
+    notif_category_id = session.query(NotificationCategory) \
+        .filter(NotificationCategory.category.ilike('%{}%'.format(category))) \
+        .first()
+    return notif_category_id.id
 
 
 def get_notif_frequencies(user_id: int):
-    pass
+    notif_frequencies = session.query(NotificationFrequency.frequency) \
+        .order_by(NotificationFrequency.id) \
+        .all()
+    return notif_frequencies
 
 
 def get_notif_frequency_id(frequency: str) -> int:
-    pass
+    notif_frequency_id = session.query(NotificationFrequency) \
+        .filter(NotificationFrequency.frequency.ilike('%{}%'.format(frequency))) \
+        .first()
+    return notif_frequency_id.id
 
 
 def is_user_notification_exists(user_plant_id: int, notif_category: int) -> bool:
-    pass
+    query = session.query(UsersNotifications) \
+        .filter(UsersNotifications.user_plant_id == user_plant_id, 
+                UsersNotifications.notif_category.ilike('%{}%'.format(notif_category)))
+    return session.query(query.exists()).scalar()
 
 
-def get_user_notification(user_plant_id: int):
-    pass
+def get_user_notifications(user_plant_id: int):
+    user_notifications = session.query(UsersNotifications) \
+        .filter(UsersNotifications.user_plant_id == user_plant_id) \
+        .all()
+    return user_notifications
 
 
 def get_user_notification_id(user_plant_id: int, notif_category: int) -> int:
-    pass
+    user_notification_id = session.query(UsersNotifications) \
+        .filter(UsersNotifications.user_plant_id == user_plant_id, 
+                UsersNotifications.notif_category.ilike('%{}%'.format(notif_category))) \
+        .first()
+    return user_notification_id
 
 
 def add_user_notification(user_plant_id: int, notif_category: int, notif_frequency: int, time: str, first_date: str):
